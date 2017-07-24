0b8b2f4e66b5db1679ff1884ad81719d8c6d2d14
Adam Page <apage224@gmail.com>
2017-07-24 14:54:06 -0400

    Updated init to include module path

--- pyvisainstrument/__init__.py
+++ pyvisainstrument/__init__.py
@@ -1,5 +1,6 @@
 __version__ = '0.8.0'
 
+from pyvisainstrument.GPIBLinkResource import GPIBLinkResource
 from pyvisainstrument.AgilentPowerSupply import AgilentPowerSupply
 from pyvisainstrument.AgilentVNA import AgilentVNA
 from pyvisainstrument.SwitchMainFrame import SwitchMainFrame
