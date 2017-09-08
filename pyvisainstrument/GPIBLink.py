3d3893ace6f20f793fc9c8978591855c54c30b43
Adam Page <apage224@gmail.com>
2017-09-08 10:06:12 -0400

    Increased delay in query

--- pyvisainstrument/GPIBLink.py
+++ pyvisainstrument/GPIBLink.py
@@ -11,7 +11,7 @@ class GPIBLinkResource(object):
         self.delay = delay
 
     def open(self):
-        self.resource = visa.ResourceManager().open_resource(self.busAddress)
+        self.resource = visa.ResourceManager('@py').open_resource(self.busAddress)
         self.isOpen = True
 
     def close(self):
