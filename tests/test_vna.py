a80f5857325cde5d403f16ba4e6ce63bb3107dbf
Adam Page <apage224@gmail.com>
2018-03-12 08:44:21 -0400

    Removed unit tests temporarily. Need address spawning dummy instrument.

--- tests/test_vna.py
+++ tests/test_vna.py
@@ -1,5 +1,6 @@
 import pytest # NOQA
 from multiprocessing import Process, Value
+import threading
 from ctypes import c_bool
 import time
 from pyvisainstrument import AgilentVNA
@@ -12,42 +13,53 @@ def runDummyInstr(done):
     baseArgs = dict(tcpAddress=TCP_IP, tcpPort=TCP_PORT, termStr='\n', bufferSize=1024)
     instr = DummyVNA(**baseArgs)
     instr.open()
-    while not done.value:
-        instr.run()
+    instr.run()
+    # while not done.value:
+    #     instr.run()
 
 class TestAgilentVNA(object):
 
     def setup_class(self):
         done = Value(c_bool, False)
-        instAddr = "TCPIP::{:s}::{:d}::SOCKET".format(TCP_IP, TCP_PORT)
-        self.dummyInst = Process(target=runDummyInstr, args=(done,))
-        self.dummyInst.start()
-        self.vna = AgilentVNA(instAddr)
+        self.vna = None
+        self.instAddr = "TCPIP::{:s}::{:d}::SOCKET".format(TCP_IP, TCP_PORT)
+        self.vnaThread = threading.Thread(target=runDummyInstr, name="VNA")
+        self.vnaThread.start()
+        # self.dummyInst = Process(target=runDummyInstr, args=(done,))
+        # self.dummyInst.start()
+        # self.vna = AgilentVNA(instAddr)
+        # self.vna.open(readTerm='\n', writeTerm='\n')
         self.done = done
 
     def teardown_class(self):
+        # self.vna.close()
+        self.vnaThread.join()
         # Set flag so instrument server knows we are done
         with self.done.get_lock():
             self.done.value = True
         # In order for inst server to handle closing must trigger new disconnect
-        if self.vna:
-            if not self.vna.isOpen:
-                self.vna.open(readTerm='\n', writeTerm='\n')
-            self.vna.close()
-        if self.dummyInst:
-            self.dummyInst.join(1)
-            if self.dummyInst.is_alive():
-                time.sleep(0.5)
-                self.dummyInst.terminate()
+        # if self.vna:
+        #    if not self.vna.isOpen:
+        #        self.vna.open(readTerm='\n', writeTerm='\n')
+        #    self.vna.close()
+        # if self.dummyInst:
+        #     self.dummyInst.join(1)
+        #     if self.dummyInst.is_alive():
+        #         time.sleep(0.5)
+        #         self.dummyInst.terminate()
 
     def setup_method(self, method):
-        self.vna.open(readTerm='\n', writeTerm='\n')
+        if not self.vna:
+            self.vna = AgilentVNA(self.instAddr)
+            self.vna.open(readTerm='\n', writeTerm='\n')
 
     def teardown_method(self, method):
-        self.vna.close()
+        pass
 
     def test_getID(self):
+
         id = self.vna.getID()
+
         assert isinstance(id, str)
 
     def test_setSweep(self):
